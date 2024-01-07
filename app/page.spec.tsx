0fb0a4ce3d80af49b5bf9a3d2dd846379db99be4
fivebirdsilver <tpdms0401@gmail.com>
2024-01-07 22:22:21 +0900

    test :: 페이지 렌더링 및 input 단위 테스트

--- app/page.spec.tsx
+++ app/page.spec.tsx
@@ -1,11 +1,27 @@
 /**
  * @jest-environment jsdom
  */
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Page from "./page";
 
-describe("메인페이지 테스트", () => {
-  it("ㅋㅋ", () => {
+describe("회원가입 테스트", () => {
+  it("비밀번호와 비밀번호 확인이 일치하지 않으면 에러메시지가 표시된다", async () => {
     render(<Page />);
+
+    // when :: 비밀번호와 비밀번호 확인이 일치하지 않음
+    const passwordInput = screen.getByLabelText("비밀번호");
+    const passwordConfirmInput = screen.getByLabelText("비밀번호 확인");
+
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(passwordConfirmInput, { target: { value: "worngpassword" } });
+
+    // const email = screen.getByLabelText("이메일");
+    // fireEvent.change(email, { target: { value: "a" } });
+    // const input = screen.getByDisplayValue("a");
+    // expect(input).toEqual("a");
+
+    // then :: 에러메시지가 표시됌
+    const errorMsg = await screen.findByTestId("error-message");
+    expect(errorMsg).toBeInTheDocument();
   });
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,52 @@
-export const metadata = {
-  title: 'App Router',
-}
+"use client";
+import { useState } from "react";
 
+interface Idata {}
 export default function Page() {
-  return <h1>App Router</h1>
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordConfirm, setPasswordConfirm] = useState<string>("");
+
+  // 회원가입 정보 제출
+  const handleOnSubmit = async (data: Idata) => {
+    console.log(data);
+  };
+  console.log("password", password);
+  console.log("passwordConfirm", passwordConfirm);
+  return (
+    <div>
+      <div>
+        <span>조직문화의 개선과 소통을 위해 지금 시작해보세요</span>
+      </div>
+      <form onSubmit={handleOnSubmit}>
+        <label htmlFor="email">이메일</label>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="이메일을 입력해주세요"
+          autoComplete="off"
+        />
+        <label htmlFor="password">비밀번호</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="비밀번호를 입력해주세요 (8 ~ 20자의 영문, 숫자, 특수문자 조합)"
+        />
+        <label htmlFor="confirmPassword">비밀번호 확인</label>
+        <input
+          id="confirmPassword"
+          type="password"
+          value={passwordConfirm}
+          placeholder="비밀번호를 한 번 더 입력해주세요"
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+        />
+        <div data-testid="error-message">비밀번호와 비밀번호 확인이 일치하지 않습니다.</div>
+        <button type="submit">가입하기</button>
+      </form>
+    </div>
+  );
 }
