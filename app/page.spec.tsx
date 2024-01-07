9b9855f9043d7ee6975c03a250f66c01365e15a3
fivebirdsilver <tpdms0401@gmail.com>
2024-01-07 18:58:31 +0900

    chore :: 불필요 파일 제거

---
+++ app/page.spec.tsx
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import Page from "./page";
+
+describe("메인페이지 테스트", () => {
+  it("ㅋㅋ", () => {
+    render(<Page />);
+  });
+});
