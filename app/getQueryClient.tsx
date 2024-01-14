a75d32123f88063046b136d777b6e43024ad4f02
fivebirdsilver <tpdms0401@gmail.com>
2024-01-14 23:10:26 +0900

    feat :: 싱글톤 인스턴스 작성

---
+++ app/getQueryClient.tsx
@@ -0,0 +1,8 @@
+// Hydrate => QueryClient의 request-scoped 싱글톤 인스턴스
+// 이렇게 하면 서로 다른 사용자 요청 간에 데이터가 공유되지 않고 요청당 한 번만 쿼리 클라이언트를 만들 수 있음
+
+import { QueryClient } from "@tanstack/react-query";
+import { cache } from "react";
+
+const getQueryClient = cache(() => new QueryClient());
+export default getQueryClient;
