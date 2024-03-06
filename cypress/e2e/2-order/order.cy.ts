f142ec9a47bdfc61b41e5f34c31d4380525bda7b
fivebirdsilver <tpdms0401@gmail.com>
2024-03-06 15:21:25 +0900

    test :: cypress 테스트 마무리

--- cypress/e2e/2-order/order.cy.ts
+++ cypress/e2e/2-order/order.cy.ts
@@ -1,3 +1,5 @@
+import list from "../../fixtures/list.json";
+
 describe("주문 테스트", () => {
   it("사용자는 주문 버튼을 눌러 주문할 수 있다.", () => {
     cy.visit("http://localhost:3000/");
@@ -7,6 +9,7 @@ describe("주문 테스트", () => {
 
     cy.url().should("include", "http://localhost:3000/order");
   });
+
   it("사용자는 음식 종류를 선택할 수 있다.", () => {
     cy.visit("http://localhost:3000/order");
     cy.intercept("GET", "http://localhost:3000/api/order").as("getData");
@@ -18,13 +21,27 @@ describe("주문 테스트", () => {
     cy.url().should("include", "http://localhost:3000/order/3");
   });
 
-  it("사용자는 선택한 음식의 상세를 선택할 수 있다.", () => {
+  it("사용자는 원하는 메뉴를 장바구니에 담고, 원하는 음식 갯수를 변경할 수 있다.", () => {
     cy.visit("http://localhost:3000/order/3");
-    cy.intercept("GET", "http://localhost:3000/api/order?id=3", {
-      fixture: "list.jon",
-    }).as("getDetail");
 
-    cy.get("[data-cy=list_3]").should("be.visible").as("listDiv");
+    // fixture을 사용해서 이미 협의된 Response가 아니 경우 체크하는 과정에서 유용하게 쓸 수 있음!
+    cy.intercept("GET", "http://localhost:3000/api/order?id=3").as("getDetail");
+
+    cy.wait("@getDetail").then((interception) => {
+      // 가로챈 요청의 응답을 얻습니다.
+      const response = interception.response?.body.data;
+
+      // list.json 파일을 읽어와서 기대값과 비교합니다.
+      cy.fixture("list.json").then((expectedResponse) => {
+        expect(response).to.deep.equal(expectedResponse);
+      });
+    });
+
+    // 동적으로 요소 아이디 가져오기
+    cy.fixture("list.json").then((list) => {
+      cy.get(`[data-cy=list_${list[0].id}]`).should("be.visible").as("listDiv");
+    });
     cy.get("@listDiv").click();
+    cy.url().should("include", `http://localhost:3000/order/3/1`);
   });
 });
