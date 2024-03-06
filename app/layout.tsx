75d3313f240903d0c4d8e5ef2be5344320fdffa1
fivebirdsilver <tpdms0401@gmail.com>
2024-03-06 10:56:32 +0900

    chore :: 네이버 지도 API 추가 및 렌더링 테스트

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "../styles/global.css";
 import TanstackProvider from "./provider";
 import Header from "./components/layout/header";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -16,6 +17,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             {children}
           </AppRouterCacheProvider>
         </TanstackProvider>
+        <Script
+          src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_ID}`}
+          strategy="beforeInteractive"
+        />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
 
 //모듈이 클라이언트 번들의 일부로 간주
 export default function Login() {
   const router = useRouter();
 
+  const mapRef = useRef(null);
+  const lat = 37.55395662140252;
+  const lng = 126.9923164220069;
+
+  useEffect(() => {
+    const { naver } = window;
+    if (mapRef.current && naver) {
+      const location = new naver.maps.LatLng(lat, lng);
+      const map = new naver.maps.Map(mapRef.current, {
+        center: location,
+        zoom: 17,
+      });
+      new naver.maps.Marker({
+        position: location,
+        map,
+      });
+    }
+  }, []);
   return (
     <div className="flex flex-col gap-5">
       <button onClick={() => router.push("/login")}>로그인</button>
@@ -12,6 +31,7 @@ export default function Login() {
       <button onClick={() => router.push("/order")} data-cy="orderBtn">
         주문
       </button>
+      {/* <div ref={mapRef} style={{ width: 500, height: 500 }}></div> */}
     </div>
   );
 }
