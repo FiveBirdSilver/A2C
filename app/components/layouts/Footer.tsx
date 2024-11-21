56436e87c837384b1433fefa8857a6bbe2ee7a4d
fivebirdsilver <tpdms0401@gmail.com>
2024-11-21 23:48:32 +0900

    [feat] 커뮤니티 탭 추가

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/navigation'
 import { FiMapPin } from 'react-icons/fi'
 import { GrHomeRounded } from 'react-icons/gr'
 import { FaRegUser } from 'react-icons/fa'
+import { FaRegComments } from 'react-icons/fa6'
 
 export default function Footer() {
   const router = useRouter()
@@ -11,6 +12,13 @@ export default function Footer() {
     <div className='fixed bottom-0 left-0 w-full border-t border-gray-100 bg-gray-50'>
       <div className='flex items-center justify-between pl-8 pr-10 mx-auto mt-2 text-lg text-gray-600 sm:max-w-screen-sm md:max-w-screen-md font-PretendardR'>
         <div className='flex flex-col items-center cursor-pointer'>
+          <FaRegComments />
+          <span className='text-[9px]'>COMMUNITY</span>
+        </div>
+        <div
+          className='flex flex-col items-center cursor-pointer'
+          onClick={() => router.push('/view')}
+        >
           <FiMapPin />
           <span className='text-[9px]'>FACILITIES</span>
         </div>
