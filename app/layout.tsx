36cf7ae7542c60184f7de2aca84afa2e013d692e
fivebirdsilver <tpdms0401@gmail.com>
2024-12-15 22:38:23 +0900

    [pref] SpeedInsights 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import Script from 'next/script'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
-import '../assets/styles/global.css'
-import Wrapper from '@/components/layouts/Wrapper'
 import Providers from './provider'
+import Wrapper from '@/components/layouts/Wrapper'
+import '../assets/styles/global.css'
 
 export default function RootLayout({
   children,
@@ -18,6 +19,7 @@ export default function RootLayout({
         <Providers>
           <Wrapper>{children}</Wrapper>
         </Providers>
+        <SpeedInsights />
         <Script
           src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_MAP_KEY}`}
           strategy='beforeInteractive'
