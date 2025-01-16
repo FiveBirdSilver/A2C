7ea8b82ac78880578ab2170ec2e92dbf5587027e
FiveBirdSilver <tpdms0401@gmail.com>
2025-01-16 15:19:44 +0900

    [pref] 미들웨어 수정

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,19 @@ import { NextResponse, NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   console.log(request)
+
+  const response = NextResponse.next()
+
+  // 헤더에 Content-Type을 설정
+  response.headers.set('Content-Type', 'text/x-component')
+
   if (request.nextUrl.pathname === '/login') {
     return NextResponse.redirect(new URL('/', request.url))
   }
-  return NextResponse.next()
+
+  return response
+}
+
+export const config = {
+  matcher: '/login', // 필요한 경로에 맞게 설정
 }
