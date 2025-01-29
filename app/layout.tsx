b7284e95672b92d004ac1271cc637cd5f95ad079
FiveBirdSilver <tpdms0401@gmail.com>
2025-01-29 19:23:22 +0900

    [pref] 모바일 확대 방지

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html suppressHydrationWarning>
       <head>
-        <meta
-          name='viewport'
-          content='initial-scale=1.0; maximum-scale=1.0; minimum-scale=1.0; user-scalable=no;'
-        />
-
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>A2C</title>
       </head>
       <body>
