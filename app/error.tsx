b2ba6c4938381289a7becfe41c8a33e7715e13f8
FiveBirdSilver <tpdms0401@gmail.com>
2025-02-03 16:10:56 +0900

    [style] 에러 페이지 UI 수정

--- app/error.tsx
+++ app/error.tsx
@@ -1,17 +1,30 @@
 'use client'
 
-import { MdError } from 'react-icons/md'
+import Image from 'next/image'
+import Button from '@/components/elements/Button.tsx'
+import { useRouter } from 'next/navigation'
 
 export default function Error({ message }: { message: string }) {
+  const router = useRouter()
+
   return (
     <div className='h-full text-center flex flex-col gap-4 items-center justify-center'>
-      <MdError className='text-red-500 text-5xl' />
-      <h6 className='text-xl'>문제가 발생했습니다</h6>
-      <p className='text-sm'>
+      <Image src={`/icons/error.png`} alt={'error'} width={120} height={50} />
+      <h6 className='text-xl font-semibold text-gray-700'>
+        문제가 발생했습니다
+      </h6>
+      <p className='text-base text-gray-700'>
         {message}
         <br />
         잠시 후 다시 시도해 주세요
       </p>
+      <div className={'grid h-10 min-w-60 w-4/12 mt-8 text-sm'}>
+        <Button
+          text={'홈으로 돌아가기'}
+          variant={'primary'}
+          onClick={() => router.push('/')}
+        />
+      </div>
     </div>
   )
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,28 @@
-import Link from 'next/link'
+'use client'
+
+import Image from 'next/image'
+import Button from '@/components/elements/Button.tsx'
+import { useRouter } from 'next/navigation'
 
 export default function NotFound() {
+  const router = useRouter()
+
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href='/'>Return Home</Link>
+    <div className='h-full text-center flex flex-col gap-4 items-center justify-center'>
+      <Image src={`/icons/404.png`} alt={'error'} width={120} height={50} />
+      <h6 className='text-xl font-semibold text-gray-700'>
+        페이지를 찾을 수 없습니다
+      </h6>
+      <p className='text-base text-gray-700'>
+        일시적으로 사용할 수 없거나 더 이상 존재하지 않는 페이지예요
+      </p>
+      <div className={'grid h-10 w-4/12 mt-8 text-sm'}>
+        <Button
+          text={'홈으로 돌아가기'}
+          variant={'primary'}
+          onClick={() => router.push('/')}
+        />
+      </div>
     </div>
   )
 }
