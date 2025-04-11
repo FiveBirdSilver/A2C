3053d0e37c95f77261c5ef04e4da86350fba84ee
fivebirdsilver <tpdms0401@gmail.com>
2025-04-11 17:07:33 +0900

    [perf] spinner => lottie 애니메이션 변경

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,18 @@
-import Spinner from '@/components/ui/Spinner'
+'use client'
+
+import dynamic from 'next/dynamic'
+import loadingAnimation from '@/public/lottie/loading.json'
+const Lottie = dynamic(() => import('react-lottie-player'), { ssr: false })
 
 export default function Loading() {
-  return <Spinner />
+  return (
+    <div className='grid h-full w-full place-items-center rounded-lg p-6 lg:overflow-visible'>
+      <Lottie
+        loop
+        animationData={loadingAnimation}
+        play
+        style={{ width: 48, height: 48 }}
+      />
+    </div>
+  )
 }
